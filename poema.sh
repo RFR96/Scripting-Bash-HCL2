389e31576f0a4001d7d9fa70c5269c7b12d783c1
Rafa <a22ferera@iesgrancapitan.org>
2023-10-05 08:47:28 +0200

    poema de subida prueba

---
+++ poema.sh
@@ -0,0 +1,12 @@
+#!/BIN/BASH
+#Autor:Rafael Fdez Relaño
+#Fecha: 5-10-2023
+#Descripcion:Poema salida por pantalla 
+
+
+
+echo "La mujer rana
+y el hombre rana
+se casaron
+y en vez de tener perdices
+tuvieron tres renacuajos."
